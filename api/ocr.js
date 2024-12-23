67434dbd49119be48e69612df72922d971bb0973
James-Lian <166276178+James-Lian@users.noreply.github.com>
2024-12-22 20:11:33 -0500

    Update ocr.js

--- api/ocr.js
+++ api/ocr.js
@@ -6,9 +6,9 @@ module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    if (req.method !== 'POST') {
-        return res.status(405).json({ error: 'Method not allowed. Use POST.' });
-    }
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }   
 
     try {
         // Parse the incoming request body for the Base64 image
