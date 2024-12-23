d8dc7fd58c5a1717ecf15fa6da0be76a39e0efa5
James-Lian <166276178+James-Lian@users.noreply.github.com>
2024-12-22 20:13:53 -0500

    Update ocr.js

--- api/ocr.js
+++ api/ocr.js
@@ -7,7 +7,8 @@ module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     if (req.method === 'OPTIONS') {
-        return res.status(200).end();
+        res.status(200).end();
+        return;
     }   
 
     try {
@@ -15,7 +16,7 @@ module.exports = async (req, res) => {
         const { imageBase64 } = req.body;
 
         if (!imageBase64) {
-        return res.status(400).json({ error: 'ImageBase64 is required.' });
+            return res.status(400).json({ error: 'ImageBase64 is required.' });
         }
 
         // Decode the Base64 string into a Buffer
